89596e509e6aba52427da18bcc32155beb20ac0c
Farooq Alwi <farooqalwi313@gmail.com>
2021-04-10 12:04:16 +0500

    separate class created for REs validation

---
+++ Classes/REs/REs_Validation.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Orion.Classes.REs
+{
+    class REs_Validation
+    {
+        public REs_Validation()
+        {
+            //to check REs
+            do
+            {
+                RE_for_integer_constant.integer_RE();
+                //RE_for_float_constant.float_RE();
+                //RE_for_char_constant.char_RE();
+                //RE_for_string_constant.string_RE();
+                //RE_for_identifier.identifier_RE();
+                //RE_for_email_validation.email_RE();
+                //RE_for_url.url_RE();
+            } while (true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Orion.Classes.REs;
-
+using System.Collections.Generic;
 
 namespace Orion
 {
@@ -7,16 +7,12 @@ namespace Orion
     {
         static void Main(string[] args)
         {
-            do
-            {
-                //RE_for_integer_constant.integer_RE();
-                //RE_for_float_constant.float_RE();
-                //RE_for_char_constant.char_RE();
-                RE_for_string_constant.string_RE();
-                //RE_for_identifier.identifier_RE();
-                //RE_for_email_validation.email_RE();
-                //RE_for_url.url_RE();
-            } while (true);
+            //to validate REs
+            new REs_Validation();
+
+            
+
+
         }
     }
 }
