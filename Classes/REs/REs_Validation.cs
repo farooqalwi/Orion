b7c302d8d565fe29506c52b29b68fee72181d712
Farooq Alwi <farooqalwi313@gmail.com>
2021-04-11 12:08:40 +0500

    floating point issue resolved in string RE

--- Classes/REs/REs_Validation.cs
+++ Classes/REs/REs_Validation.cs
@@ -8,10 +8,10 @@ namespace Orion.Classes.REs
             //to check REs
             do
             {
-                RE_for_integer_constant.integer_RE();
+                //RE_for_integer_constant.integer_RE();
                 //RE_for_float_constant.float_RE();
                 //RE_for_char_constant.char_RE();
-                //RE_for_string_constant.string_RE();
+                RE_for_string_constant.string_RE();
                 //RE_for_identifier.identifier_RE();
                 //RE_for_email_validation.email_RE();
                 //RE_for_url.url_RE();
