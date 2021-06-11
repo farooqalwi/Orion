1275df685d365d0f2851950d29949df1276c30ba
Farooq Alwi <farooqalwi313@gmail.com>
2021-06-11 10:10:42 +0500

    Non-terminals empty methods created

--- Classes/SyntaxAnalyzer/Syntax_Analyzer.cs
+++ Classes/SyntaxAnalyzer/Syntax_Analyzer.cs
@@ -1,12 +1,392 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Orion.Classes.SyntaxAnalyzer
 {
     class Syntax_Analyzer
     {
+        public static bool OE(string token)
+        {
+            return false;
+        }
+
+        public static bool OE_(string token)
+        {
+            return false;
+        }
+
+        public static bool AE(string token)
+        {
+            return false;
+        }
+        public static bool AE_(string token)
+        {
+            return false;
+        }
+
+        public static bool RE(string token)
+        {
+            return false;
+        }
+
+        public static bool RE_(string token)
+        {
+            return false;
+        }
+
+        public static bool E(string token)
+        {
+            return false;
+        }
+
+        public static bool E_(string token)
+        {
+            return false;
+        }
+
+        public static bool T(string token)
+        {
+            return false;
+        }
+
+        public static bool T_(string token)
+        {
+            return false;
+        }
+
+        public static bool F(string token)
+        {
+            return false;
+        }
+
+        public static bool F_(string token)
+        {
+            return false;
+        }
+
+        public static bool F_list(string token)
+        {
+            return false;
+        }
+
+        //in CFG it is const not CONST
+        public static bool CONST(string token)
+        {
+            return false;
+        }
+
+        public static bool PL(string token)
+        {
+            return false;
+        }
+
+        public static bool PL2(string token)
+        {
+            return false;
+        }
+
+        public static bool assign_st(string token)
+        {
+            return false;
+        }
+
+        public static bool assign_opr(string token)
+        {
+            return false;
+        }
+
+        public static bool X(string token)
+        {
+            return false;
+        }
+
+        public static bool X2(string token)
+        {
+            return false;
+        }
+
+        public static bool X3(string token)
+        {
+            return false;
+        }
+
+        public static bool index(string token)
+        {
+            return false;
+        }
+
+        public static bool inc_dec_st(string token)
+        {
+            return false;
+        }
+
+        public static bool inc_dec(string token)
+        {
+            return false;
+        }
+
+        public static bool if_else(string token)
+        {
+            return false;
+        }
+
+        //in CFG it is else not _else
+        public static bool _else(string token)
+        {
+            return false;
+        }
+
+        public static bool obj_dec(string token)
+        {
+            return false;
+        }
+
+        public static bool obj_dec_list(string token)
+        {
+            return false;
+        }
+
+        public static bool obj_dec_list1(string token)
+        {
+            return false;
+        }
+
+        public static bool obj_dec_list2(string token)
+        {
+            return false;
+        }
+
+        public static bool Class_dec(string token)
+        {
+            return false;
+        }
+
+        public static bool Non_AM(string token)
+        {
+            return false;
+        }
+
+        public static bool extends(string token)
+        {
+            return false;
+        }
+
+        public static bool dec(string token)
+        {
+            return false;
+        }
+
+        public static bool init(string token)
+        {
+            return false;
+        }
+
+        public static bool dec_list(string token)
+        {
+            return false;
+        }
+
+        public static bool func_def(string token)
+        {
+            return false;
+        }
+
+        public static bool AM(string token)
+        {
+            return false;
+        }
+
+        public static bool func_ret_type(string token)
+        {
+            return false;
+        }
+
+        public static bool ret_line(string token)
+        {
+            return false;
+        }
+
+        public static bool ret(string token)
+        {
+            return false;
+        }
+
+        public static bool func_call(string token)
+        {
+            return false;
+        }
+
+        public static bool Y(string token)
+        {
+            return false;
+        }
+
+        public static bool for_st(string token)
+        {
+            return false;
+        }
+
+        public static bool c1(string token)
+        {
+            return false;
+        }
+
+        public static bool c2(string token)
+        {
+            return false;
+
+        }
+
+        public static bool While_st(string token)
+        {
+            return false;
+
+        }
+
+        public static bool arr1(string token)
+        {
+            return false;
+        }
+
+        public static bool arr2(string token)
+        {
+            return false;
+        }
+
+        public static bool arr3(string token)
+        {
+            return false;
+        }
+
+        public static bool arr4(string token)
+        {
+            return false;
+
+        }
+
+        public static bool arr5(string token)
+        {
+            return false;
+
+        }
+
+        public static bool switch_case(string token)
+        {
+            return false;
+        }
+
+        //in CFG it is case not _case
+        public static bool _case(string token)
+        {
+            return false;
+        }
+
+        public static bool case1(string token)
+        {
+            return false;
+        }
+
+        //in CFG it is default not _default
+        public static bool _default(string token)
+        {
+            return false;
+
+        }
+
+        public static bool try_catch(string token)
+        {
+            return false;
+
+        }
+
+        public static bool S(string token)
+        {
+            return false;
+
+        }
+
+        public static bool CB(string token)
+        {
+            return false;
+
+        }
+
+        public static bool defs(string token)
+        {
+            return false;
+
+        }
+
+        public static bool fn(string token)
+        {
+            return false;
+
+        }
+
+        public static bool fn1(string token)
+        {
+            return false;
+
+        }
+
+        public static bool fn2(string token)
+        {
+            return false;
+
+        }
+
+        public static bool interface_dec(string token)
+        {
+            return false;
+
+        }
+
+        public static bool method(string token)
+        {
+            return false;
+
+        }
+
+        public static bool implem_dec(string token)
+        {
+            return false;
+
+        }
+
+        public static bool method_implem(string token)
+        {
+            return false;
+
+        }
+
+        public static bool SST(string token)
+        {
+            return false;
+
+        }
+
+        public static bool COM(string token)
+        {
+            return false;
+
+        }
+
+        public static bool call(string token)
+        {
+            return false;
+
+        }
+
+        public static bool body(string token)
+        {
+            return false;
+
+        }
+
+        public static bool MST(string token)
+        {
+            return false;
+
+        }
+
     }
 }
